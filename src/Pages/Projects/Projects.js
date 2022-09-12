9f1d836cb9083666b40f0fe6d203e4ae1d8827a0
Mauro <MauroMarchesan88@gmail.com>
2022-09-12 11:37:25 -0300

    Comments on projects, FullStack on maintenance

--- src/Pages/Projects/Projects.js
+++ src/Pages/Projects/Projects.js
@@ -10,29 +10,41 @@ import './index.scss';
 function Projects() {
   return (
     <div className='Projects'>
+        <p>Here are some examples of different things i've developed, there's a lot more that i've made over at github (30+repos and growing), but i've selected these as examples of different final products.
+          There's an E-Commerce example, as a Todo-List with a twist, a game and a Fullstack app (this one was "hurt" removing free tier so, might take some time to be deployed again). All of them where coded using Airbnb Eslint.
+        </p>
       <div className='projects'>
-
         <div className='project1'>
           <div className='project-wrapper'>
-            <img src={plaHldrBG} alt='screenFromGame' />
+            <img src={plaHldrBG} alt='screenFromNew' />
             <label htmlFor='project1'>
-              <a href="https://trybe-wallet-mauro.netlify.app/" id='project1' target="_blank" rel="noreferrer">Live</a>
+              {/* <a href="https://trybe-wallet-mauro.netlify.app/" id='project1' target="_blank" rel="noreferrer">Live</a> */}
             </label>
             <label htmlFor='project1b'>
-              <a href='https://github.com/MauroMarchesan88/Projeto-TrybeWallet' id='project1b' target="_blank" rel="noreferrer">Code</a>
+              {/* <a href='https://github.com/MauroMarchesan88/Projeto-TrybeWallet' id='project1b' target="_blank" rel="noreferrer">Code</a> */}
             </label>
+            <div className='projectDesc'>
+              <p>Fullstack Project Example:
+                In maintenance, deploy in Heroku needed to move.
+              </p>
+            </div>
           </div>
         </div>
 
         <div className='project2'>
           <div className='project-wrapper'>
-            <img src={shoppingCart} alt='screenFromGame' />
+            <img src={shoppingCart} alt='screenFromCart' />
             <label htmlFor='project2'>
               <a href="https://shopping-cart-mauro.netlify.app//" id='project2' target="_blank" rel="noreferrer">Live</a>
             </label>
             <label htmlFor='project2b'>
               <a href='https://github.com/MauroMarchesan88/Projeto-Shopping-Cart' id='project2b' target="_blank" rel="noreferrer">Code</a>
             </label>
+            <div className='projectDesc'>
+              <p>E-Commerce Project Example:
+                This project was made with pure HTML, CSS and JS. All the information is fetched from an external API and used to render a shopping cart with prices and calculations for total price.
+              </p>
+            </div>
           </div>
         </div>
 
@@ -45,6 +57,11 @@ function Projects() {
             <label htmlFor='project3b'>
               <a href='https://github.com/MauroMarchesan88/Fight-Game' id='project3b' target="_blank" rel="noreferrer">Code</a>
             </label>
+            <div className='projectDesc'>
+              <p>Side-Project:
+                This game was a side project that i used to learn more about canvas, it's a simple game with animations nothing fancy but it was fun, easy to develop and helped me better understand how to render animations.
+              </p>
+            </div>
           </div>
         </div>
 
@@ -57,6 +74,12 @@ function Projects() {
             <label htmlFor='project4b'>
               <a href='https://github.com/MauroMarchesan88/Projeto-TrybeWallet' id='project4b' target="_blank" rel="noreferrer">Code</a>
             </label>
+            <div className='projectDesc'>
+              <p>Todo with a twist!:
+                I was tired of Todo Lists and this one was a nice example with some extra, it fetchs exchange information from an external API to add on top of the basic Todo-List.
+                I used React and was learning about Redux, so it has Redux for state management.
+              </p>
+            </div>
           </div>
         </div>
 
